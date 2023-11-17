6e71a3159849eef950e86913f6c1a71ff7463817
Franco Capelo <115761575+FrancoCapelo29@users.noreply.github.com>
2023-11-17 10:53:05 -0500

    projecto-create

---
+++ src/main/kotlin/com/suda/TGC/TgcApplication.kt
@@ -0,0 +1,11 @@
+package com.suda.TGC
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TgcApplication
+
+fun main(args: Array<String>) {
+	runApplication<TgcApplication>(*args)
+}
---
+++ src/test/kotlin/com/suda/TGC/TgcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.suda.TGC
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TgcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
